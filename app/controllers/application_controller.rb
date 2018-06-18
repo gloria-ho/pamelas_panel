1a72fbe6ce298664ebe059a2551da4531942655c
Gloria H <silverdragonia@gmail.com>
2018-06-18 11:37:20 -0400

    added user settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:f_name, :l_name, :access, :birthday])
+    #in keys you list all the input you want to accept.
 end
