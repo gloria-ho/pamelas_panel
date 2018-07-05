c7d71c49126d9f93e0f52e81c3131ea0a5da734a
Gloria H <silverdragonia@gmail.com>
2018-07-05 11:03:28 -0400

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def custom_bootstrap_flash
+  def custom_bootstrap_flash
     flash_messages = []
     flash.each do |type, message|
       type = 'success' if type == 'notice'
